8cbdb092de84cda56e318374e4f18024240a1b0f
Özkan Yılmaz <dev.ozkanyilmaz@gmail.com>
2026-01-06 15:47:56 +0300

    Add project files.

---
+++ ResumeProject/Entities/Testimonial.cs
@@ -0,0 +1,12 @@
+﻿namespace ResumeProject.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string NameSurname { get; set; }
+        public string TitleAndCompany { get; set; }
+        public string CommentDetail { get; set; }
+        public string ImageUrl { get; set; }
+        public bool isConfirm { get; set; }
+    }
+}
