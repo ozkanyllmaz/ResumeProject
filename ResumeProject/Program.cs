c118e621deca8ac5251ee5e86fc38bad9eddd86b
Özkan Yılmaz <dev.ozkanyilmaz@gmail.com>
2026-01-21 14:27:45 +0300

    Default Index partial lara bölündü ve migration uygulandı

--- ResumeProject/Program.cs
+++ ResumeProject/Program.cs
@@ -1,6 +1,9 @@
+using ResumeProject.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ResumeContext>(); //sonradan eklendi
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
--- ResumeProject/ViewComponents/DefaultViewComponents/_DefaultExperienceComponentPartial.cs
+++ ResumeProject/ViewComponents/DefaultViewComponents/_DefaultExperienceComponentPartial.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ResumeProject.Context;
 
 namespace ResumeProject.ViewComponents.DefaultViewComponents
 {
     public class _DefaultExperienceComponentPartial : ViewComponent
     {
+        private readonly ResumeContext _context;
+
+        public _DefaultExperienceComponentPartial(ResumeContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _context.Experiences.ToList();
+            return View(values);
         }
     }
 }
